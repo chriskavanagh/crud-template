f2c9cc212dcb3c2df5ba9a1f0b2582661074345c
Chris Kavanagh <ckava3@gmail.com>
2020-01-31 22:31:19 -0500

    getItems App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /* useEffect(() => {
+    Axios.get("http://localhost:3001/crud")
+      .then(response => setItems(response.data))
+      .catch(err => console.log(err));
+  }, []); */
+
+  useEffect(() => {
+    async function getItems() {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const { data } = await Axios.get("http://localhost:3001/crud");
+        setItems(data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    }
+    getItems();
+    // setTimeout(getItems, 8000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          <ul>
+            {items.map((item, idx) => (
+              <li key={idx}>{item.first}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
